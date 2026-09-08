bc12288c9a9f9f28947c365acbc6931467ef969c
BAR2 <benoit.arcache@gmail.com>
2026-09-08 23:20:27 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
 
+#Ouvre deux autre terminaux
+mate-terminal & mate-terminal &
+
+#Ajout d'alias dans le .bash_aliases
+echo "alias c='clear'" >> ~/.bash_aliases
+
+
+#Téléchargement de ressources utiles
 wget https://github.com/peass-ng/PEASS-ng/releases/latest/download/linpeas.sh
-#wget https://github.com/peass-ng/PEASS-ng/releases/latest/download/Winpeas.sh
+wget https://github.com/peass-ng/PEASS-ng/blob/master/winPEAS/winPEASbat/winPEAS.bat
+wget https://github.com/peass-ng/PEASS-ng/blob/master/winPEAS/winPEASps1/winPEAS.ps1
+wget https://github.com/peass-ng/PEASS-ng/releases/latest/download/winPEASany_ofs.exe
+
 # ajout d'alias pour lancer serveur web
 # 
-# 
+# scan nmap full
+#sudo nmap -sSV -Pn -vvv -p- --open --reason -oA ${IP}_FULL_ports_SYN ${IP} >/dev/null 
