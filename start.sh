a29dccef4fcfc0c3d76b8adf48e2f0847ee39236
BAR2 <benoit.arcache@gmail.com>
2026-09-08 22:37:09 +0200

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+wget https://github.com/peass-ng/PEASS-ng/releases/latest/download/linpeas.sh
+#wget https://github.com/peass-ng/PEASS-ng/releases/latest/download/Winpeas.sh
+# ajout d'alias pour lancer serveur web
+# 
+# 
